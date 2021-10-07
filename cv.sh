5f845a4312b640b0c32fa405c4b1aec8a7a375fb
Bogdan <bcqdan@gmail.com>
2021-10-07 19:27:03 +0200

    x

---
+++ cv.sh
@@ -0,0 +1,6 @@
+/usr/local/texlive/2021/bin/universal-darwin/pdflatex Bogdan\ Bălan\ CV.tex && \
+open Bogdan\ Bălan\ CV.pdf && \
+rm *.aux && \
+rm *.log && \
+rm *.out
+
--- latex-it-cv.sh
+++
@@ -1,6 +0,0 @@
-/usr/local/texlive/2015/bin/x86_64-darwin/pdflatex Bogdan\ Bălan\ CV.tex && \
-open Bogdan\ Bălan\ CV.pdf && \
-rm *.aux && \
-rm *.log && \
-rm *.out
-

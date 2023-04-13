ad57e7da1ae4a66a9251436eed25a12ef77cc19e
Bogdan Balan <bcqdan@gmail.com>
2023-04-13 09:31:00 +0300

    progress

--- cv.sh
+++ cv.sh
@@ -1,4 +1,6 @@
-/usr/local/texlive/2021/bin/universal-darwin/pdflatex Bogdan\ Balan\ CV.tex && \
+# /usr/local/texlive/2021/bin/universal-darwin/pdflatex Bogdan\ Balan\ CV.tex && \
+
+/Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin/pdflatex Bogdan\ Balan\ CV.tex && \
 open Bogdan\ Balan\ CV.pdf && \
 rm *.aux && \
 rm *.log && \
